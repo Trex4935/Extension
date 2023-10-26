ca2c3144dcc41d605e87722ff1e489c5f8a4f880
Canthv0 <matbyrd@forthebyrds.com>
2023-10-26 18:45:48 -0400

    Initial Set of Motor Extensions

---
+++ src/main/java/frc/robot/extension/SparkMax.java
@@ -0,0 +1,76 @@
+package frc.robot.extension;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.SparkMaxPIDController.AccelStrategy;
+
+public class SparkMax {
+    /**
+     * Creates a default CAN brushless SparkMax motor. Brushless is the one we use
+     * most often.
+     * 
+     * @param CANID CAN ID of the sparkmax.
+     * @return
+     */
+    public static CANSparkMax createDefaultCANSparkMax(int CANID) {
+        CANSparkMax sparkValue = new CANSparkMax(CANID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        sparkValue.setIdleMode(IdleMode.kBrake);
+        return sparkValue;
+    }
+
+    /**
+     * Adds SmartMotion functionality and a PID controller
+     * 
+     * @param motorObject
+     *                        The motor object
+     * @param kP
+     *                        The proportional gain
+     * @param kI
+     *                        The integral gain
+     * @param kD
+     *                        The derivative gain
+     * @param kFF
+     *                        The feed forward
+     * @param minVelocity
+     *                        The minimum velocity desired
+     * @param maxVelocity
+     *                        The maximum velocity desired
+     * @param maxAcceleration
+     *                        The maximum acceleration desired
+     * @param error
+     *                        The amount of closed-loop error allowed
+     */
+    public static CANSparkMax configPIDwithSmartMotion(CANSparkMax motorObject, double kP, double kI, double kD,
+            double kFF, double minVelocity, double maxVelocity, double maxAcceleration, double error) {
+
+        /* Creates a PID controller for the SparkMax. */
+        SparkMaxPIDController SparkMaxPID = motorObject.getPIDController();
+
+        /* Enables SmartMotion for the PID controller */
+        SparkMaxPID.setReference(0, CANSparkMax.ControlType.kSmartMotion, 0);
+
+        /*
+         * Sets the acceleration strat (May not do anything since trapezoidal is the
+         * only one now?)
+         */
+        SparkMaxPID.setSmartMotionAccelStrategy(AccelStrategy.kTrapezoidal, 0);
+
+        /* Sets motion constraints for the PID controller */
+        SparkMaxPID.setSmartMotionMinOutputVelocity(minVelocity, 0);
+        SparkMaxPID.setSmartMotionMaxVelocity(maxVelocity, 0);
+        SparkMaxPID.setSmartMotionMaxAccel(maxAcceleration, 0);
+        SparkMaxPID.setSmartMotionAllowedClosedLoopError(error, 0);
+
+        /* Gives values to PID controllers */
+        SparkMaxPID.setP(kP);
+        SparkMaxPID.setI(kI);
+        // Not sure if we need to do something with this
+        SparkMaxPID.setIZone(0, 0);
+        SparkMaxPID.setD(kD);
+        SparkMaxPID.setFF(kFF);
+
+        return motorObject;
+    }
+}
---
+++ src/main/java/frc/robot/extension/Talon.java
@@ -0,0 +1,107 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.extension;
+
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+/** Add your docs here. */
+public class Talon {
+
+    // private static final int Timeout = 20;
+
+    public static class DefaultConfiguration {
+
+        // Set Motor Brake mode
+        public NeutralMode neutralMode = NeutralMode.Brake;
+
+        // Input dead band
+        public double neutralDeadband = 0.04;
+
+        
+        // Motor ramp when using open loop
+        public double openLoopRamp = 0;
+
+        // Set motor limits
+        //// normal output forward and reverse = 0% ... i.e. stopped
+        public double nominalOutputForward = 0;
+        public double nominalOutputReverse = 0;
+
+        //// Max output forward and reverse = 100%
+        public double peakOutputForward = 1;
+        public double peakOutputReverse = -1;
+
+        public boolean enableCurrentLimit = false;
+        public SupplyCurrentLimitConfiguration currLimitCfg = new SupplyCurrentLimitConfiguration(enableCurrentLimit,
+                20, 60, 0.2);
+
+    }
+
+    private static DefaultConfiguration defaultConfig = new DefaultConfiguration();
+
+    //
+    /**
+     * create a CANTalon with the default (out of the box) configuration
+     *
+     * @param id
+     *           CAN ID of the motor to configure
+     * 
+     * @return Configured WPI_TalonFX motor
+     */
+    public static WPI_TalonSRX createDefaultTalon(int id) {
+        return createTalon(id, defaultConfig);
+    }
+
+    /**
+     * Configures a Talon with MotionMagic configuration
+     *
+     * @param id
+     *               CAN ID of the motor to configure
+     * 
+     * @param config
+     *               Config object to use to set values on the motor
+     * 
+     * @return Configured WPI_TalonFX motor
+     */
+    public static WPI_TalonSRX createTalon(int id, DefaultConfiguration config) {
+        WPI_TalonSRX talon = new WPI_TalonSRX(id);
+
+        talon.configFactoryDefault();
+        talon.set(ControlMode.PercentOutput, 0.0);
+        talon.setNeutralMode(config.neutralMode);
+        talon.configOpenloopRamp(config.openLoopRamp);
+
+        talon.configNominalOutputForward(config.nominalOutputForward);
+        talon.configNominalOutputReverse(config.nominalOutputReverse);
+
+        talon.configPeakOutputForward(config.peakOutputForward);
+        talon.configPeakOutputReverse(config.peakOutputReverse);
+
+        talon.configSupplyCurrentLimit(config.currLimitCfg);
+
+       
+        talon.configPeakCurrentLimit(15);
+		talon.configPeakCurrentDuration(0);
+		talon.configContinuousCurrentLimit(10);
+
+        boolean _currentLimEn = true;
+       
+		talon.enableCurrentLimit(_currentLimEn); // Honor initial setting
+
+		/* setup a basic closed loop */
+    
+		talon.setNeutralMode(NeutralMode.Brake); // Netural Mode override 
+        talon.configSelectedFeedbackSensor(  FeedbackDevice.QuadEncoder, 0,0);      
+
+        /* Ensure Sensor is in phase, else closed loop will not work.
+         * Positive Sensor should match Motor Positive output (Green LED)
+         */
+        talon.setSensorPhase(true);
+
+        // Return the configured motor object
+        return talon;
+    }
+
+}
